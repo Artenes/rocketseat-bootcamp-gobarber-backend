11207af12dc2f6c76089caa474787b740801c55d
Artenes Nogueira <artenesama@gmail.com>
2019-08-25 16:30:24 -0400

    Register models in application

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jhon Doe',
+    email: 'jhon.doe@doe.com',
+    password_hash: '1234567890',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
