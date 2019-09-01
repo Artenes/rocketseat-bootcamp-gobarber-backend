9419505256237e7512521b4f46960acbf824ff0a
Artenes Nogueira <artenesama@gmail.com>
2019-09-01 09:12:50 -0400

    Adds pagination to appointments list

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,10 +7,17 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      // (1 - 1) * 20 = 0, then no appointment will be skipped
+      // (2 - 1) * 20 = 20, then will skip 20 appointments
+      // and so on...
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
