b6810df2c8559c0945fb7baa711dc65e7f3ac9c0
Artenes Nogueira <artenesama@gmail.com>
2019-09-01 12:20:59 -0400

    Sends email when appointment is canceled
    We make use of a library to send the email:
    yarn add nodemailer
    
    We create a file at /src/config/mail.js to store some configurations info.
    
    Then we make the class /src/lib/Mail.js to serve as a wrapper for the library and to properly load the configuration into the library so we do not need to keep loading it everytime we have to use it.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '92b1f3cd88b2cf',
+    pass: '51362065305381',
+  },
+  default: {
+    from: 'GoBarber team <noreply@gobarber.com>',
+  },
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+import mailConfig from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+
+    this.transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      // there are some strategies to send email that does not make use of auth
+      auth: auth.user ? auth : null,
+    });
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...mailConfig.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
