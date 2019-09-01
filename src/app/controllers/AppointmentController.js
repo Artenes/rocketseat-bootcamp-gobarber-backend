772928f46260be3dda0eca1a28fcf99b6be1cd64
Artenes Nogueira <artenesama@gmail.com>
2019-09-01 17:30:07 -0400

    Adds more information to Appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       // (1 - 1) * 20 = 0, then no appointment will be skipped
       // (2 - 1) * 20 = 20, then will skip 20 appointments
